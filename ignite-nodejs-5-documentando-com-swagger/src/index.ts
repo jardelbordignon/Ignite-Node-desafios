a3e23cb37148b18aa3f81182a6d4e47465f9245b
Jardel <jardel1101@gmail.com>
2021-03-18 23:53:19 -0300

    Swagger documentation sample

---
+++ ignite-nodejs-5-documentando-com-swagger/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import swagger from "swagger-ui-express";
+
+import { usersRoutes } from "./routes/users.routes";
+import swaggerJson from "./swagger.json";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerJson));
+
+app.use("/users", usersRoutes);
+
+export { app };
